161554971795321d5203926a8a90d7d8642f3b8a
kamilpikula <pikusix1@o2.pl>
2020-06-24 16:25:14 +0200

    first view index items

--- lib/todo_list_web/views/item_view.ex
+++ lib/todo_list_web/views/item_view.ex
@@ -1,3 +1,19 @@
 defmodule TodoListWeb.ItemView do
   use TodoListWeb, :view
+
+  # add class "completed" to a list item if item.status = 1
+  def complete(item) do
+    case item.status do
+      1 -> "completed"
+      _ -> "" # empty string means empty class so no style
+    end
+  end
+
+  # add "checked" to input if item.status = 1
+  def checked(item) do
+    case item.status do
+      1 -> "checked"
+      _ -> ""
+    end
+  end
 end
