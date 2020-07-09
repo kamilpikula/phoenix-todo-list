cf632659a1f5d2489065138551ee6c1579e89c1e
kamilpikula <pikusix1@o2.pl>
2020-07-09 13:09:59 +0200

    create seeds items

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -5,6 +5,16 @@
 # Inside the script, you can read and write to any of your
 # repositories directly:
 #
+  alias TodoList.Todo
+
+  items = [
+    %{text: "active task", status: 0},
+    %{text: "completed task", status: 1}
+  ]
+
+  Enum.each(items, fn(item) ->
+    Todo.create_item(item)
+  end)
 #     TodoList.Repo.insert!(%TodoList.SomeSchema{})
 #
 # We recommend using the bang functions (`insert!`, `update!`
